831db0c8027a21c35f2a85477efa7d5c3201b889
junkim123123 <junkimfrom82@gmail.com>
2026-01-04 02:44:54 -0600

    chore: remove nexsupply

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,12 +1,9 @@
 import HomeDeck from "@/components/home/HomeDeck"
-import { loadProductManifest } from "@/lib/productManifest"
 
 export default function HomePage() {
-  const products = loadProductManifest();
-  
   return (
     <main className="bg-white">
-      <HomeDeck products={products} />
+      <HomeDeck />
     </main>
   )
 }
